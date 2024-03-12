2a64fec47f1fffb19a2c1b634233ee8b2461aa72
kenilkevadiya <kskevadiya2002@gmail.com>
2024-03-12 19:07:28 -0300

    spring boot files added

---
+++ studentrecordsystem/src/main/java/com/kenilsk/studentrecordsystem/StudentrecordsystemApplication.java
@@ -0,0 +1,13 @@
+package com.kenilsk.studentrecordsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentrecordsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentrecordsystemApplication.class, args);
+	}
+
+}
---
+++ studentrecordsystem/src/test/java/com/kenilsk/studentrecordsystem/StudentrecordsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kenilsk.studentrecordsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentrecordsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
