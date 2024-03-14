7e976479939a491004003439b3a7fb240f29aaea
kenilkevadiya <kskevadiya2002@gmail.com>
2024-03-13 22:37:57 -0300

    getting data from database (GetMapping)

--- studentrecordsystem/src/main/java/com/kenilsk/studentrecordsystem/controller/StudentController.java
+++ studentrecordsystem/src/main/java/com/kenilsk/studentrecordsystem/controller/StudentController.java
@@ -3,10 +3,9 @@ package com.kenilsk.studentrecordsystem.controller;
 import com.kenilsk.studentrecordsystem.model.Student;
 import com.kenilsk.studentrecordsystem.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/student")
@@ -18,5 +17,10 @@ public class StudentController {
     public String add(@RequestBody Student student) {
         studentService.saveStudent(student);
         return "New student added";
+    }
+
+    @GetMapping("/getAll")
+    public List<Student> getAllStudents() {
+        return studentService.getAllStudents();
     }
 }
--- studentrecordsystem/src/main/java/com/kenilsk/studentrecordsystem/service/StudentService.java
+++ studentrecordsystem/src/main/java/com/kenilsk/studentrecordsystem/service/StudentService.java
@@ -2,6 +2,10 @@ package com.kenilsk.studentrecordsystem.service;
 
 import com.kenilsk.studentrecordsystem.model.Student;
 
+import java.util.List;
+
 public interface StudentService {
     public Student saveStudent(Student student);
+
+    public List<Student> getAllStudents();
 }
